ba0daa56c0771be6588af492807209c50abfc4b2
Ahmed Abdullah Khaled <csewebmail@gmail.com>
2021-07-18 21:55:29 +0600

    create seeding for fake data

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -5,8 +5,13 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(\App\Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
     return [
-        //
+        'title' => ucfirst($title),
+        'slug' => \Str::slug($title),
+        'body' => $faker->text,
+        'category_id' => \App\Category::all()->random()->id,
+        'user_id' => \App\User::all()->random()->id,
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //factory(\App\User::class, 7)->create();
+        //factory(\App\Category::class, 10)->create();
+        factory(\App\Question::class, 50)->create();
     }
 }
