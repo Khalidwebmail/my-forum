c0983c72c890a527c888b7e1e921e9c949f02096
Ahmed Abdullah Khaled <csewebmail@gmail.com>
2021-07-18 23:49:46 +0600

    Create question crud using api

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Question::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Question::create($request->all());
+        return response()->json(['success'=>'Question created'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  obj  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return $question;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        $question->update($request->all());
+        return response()->json(['success'=>'Question updated'], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+        return response()->json(['success'=>'Question deleted'], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,20 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * Category route
+ */
 Route::get('/categories', 'CategoryController@index')->name('category.list');
 Route::get('/categories/{category}', 'CategoryController@show')->name('category.show');
 Route::post('/categories/store', 'CategoryController@store')->name('category.store');
 Route::patch('/categories/{category}/update', 'CategoryController@update')->name('category.update');
 Route::delete('/categories/{category}/delete', 'CategoryController@destroy')->name('category.delete');
+
+/**
+ * Question route
+ */
+Route::get('/questions', 'QuestionController@index')->name('question.list');
+Route::get('/questions/{question}', 'QuestionController@show')->name('question.show');
+Route::post('/questions/store', 'QuestionController@store')->name('question.store');
+Route::patch('/questions/{question}/update', 'QuestionController@update')->name('question.update');
+Route::delete('/questions/{question}/delete', 'QuestionController@destroy')->name('question.delete');
