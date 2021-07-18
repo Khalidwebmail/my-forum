402cee329a5d75dc7e66e8b298f3370ca57df746
Ahmed Abdullah Khaled <csewebmail@gmail.com>
2021-07-18 21:27:46 +0600

    make model relationship

--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    protected $fillable = ['question_id', 'user_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function question()
+    {
+        return $this->belongsTo(Question::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function like()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
