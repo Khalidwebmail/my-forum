30cfb6dcf34e94982b3e27625eb6c2b106473a54
Ahmed Abdullah Khaled <csewebmail@gmail.com>
2021-07-20 00:37:32 +0600

    create home component

---
+++ resources/js/components/AppHome.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <toolbar-component></toolbar-component>
+        <footer-component></footer-component>
+    </div>
+</template>
+
+<script>
+
+import ToolbarComponent from "./ToolbarComponent";
+import FooterComponent from "./FooterComponent";
+export default {
+    name: "AppHome",
+    components: {FooterComponent, ToolbarComponent}
+}
+</script>
+
+<style scoped>
+
+</style>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,22 +5,14 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
     <body>
         <div id="app">
             <v-app>
-                <v-toolbar>
-                    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-                    <v-app-bar-title>Title</v-app-bar-title>
-                    <v-spacer></v-spacer>
-                    <v-toolbar-items class="hidden-sm-and-down">
-                        <v-btn text>Link one</v-btn>
-                        <v-btn text>Link two</v-btn>
-                        <v-btn text>Link three</v-btn>
-                    </v-toolbar-items>
-                </v-toolbar>
+                <app-home></app-home>
             </v-app>
         </div>
     </body>
